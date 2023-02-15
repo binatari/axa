d3d93a953dc06dc23a2149ae217205cd290decf2
unknown <emmanuelamiteye9@gmail.comgit config --global user.name Emmanuel>
2023-02-15 23:48:39 +0100

    New change

--- src/components/partials/FeaturesBlocks.jsx
+++ src/components/partials/FeaturesBlocks.jsx
@@ -19,7 +19,7 @@ function FeaturesBlocks() {
           </div>
 
           {/* Items */}
-          <div className="max-w-sm mx-auto grid gap-6 md:grid-cols-2 lg:grid-cols-3 items-start md:max-w-2xl lg:max-w-none">
+          <div data-aos="fade-up" className="max-w-sm mx-auto grid gap-6 md:grid-cols-2 lg:grid-cols-3 items-start md:max-w-2xl lg:max-w-none">
 
             {/* 1st item */}
             <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl">
